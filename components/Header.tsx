75f40f6da4bf8f7ebf0e755ec94e7ab43200a9e0
Media Department <intelliface.tech@gmail.com>
2026-06-05 23:23:00 +0300

    Update margin for header logo edition text

--- components/Header.tsx
+++ components/Header.tsx
@@ -67,7 +67,7 @@ export default function Header() {
               <span className="font-outfit font-bold text-[13px] text-white tracking-tight">
                 {t('footer.copyright')}
               </span>
-              <span className="text-[9px] text-white/30 tracking-[0.16em] uppercase mt-1">
+              <span className="text-[9px] text-white/30 tracking-[0.16em] uppercase mt-[2px]">
                 {t('nav.edition')}
               </span>
             </div>
